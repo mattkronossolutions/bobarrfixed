d9bf7b7a302f8fa4153a2ad3fdfb81fe8dd7e760
mattkronossolutions <matthew@kronossolutions.org>
2026-05-27 20:56:37 -0700

    1

--- packages/web/utils/api-url.ts
+++ packages/web/utils/api-url.ts
@@ -1,6 +1,2 @@
-import getConfig from 'next/config';
-
-const { publicRuntimeConfig } = getConfig() || {};
 const host = typeof window === 'undefined' ? 'api' : window.location.hostname;
-export const apiURL =
-  publicRuntimeConfig?.WEB_UI_API_URL || `http://${host}:4000`;
+export const apiURL = process.env.WEB_UI_API_URL || `http://${host}:4000`;
